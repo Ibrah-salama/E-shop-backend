d1d4a04981f74c7b2260b48931dddb189ad2b7ab
i-salama <ibrah.salama@outlook.com>
2021-09-24 08:44:16 +0200

    add some routes to product route

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const CategoryModel = require("../models/category");
 const router = express.Router();
-const ProductModel = require("../models/product")
+const ProductModel = require("../models/product");
+const mongoose = require('mongoose')
 
 router.get("/", async (req, res, next) => {
-  const prod = await ProductModel.find();
+  const prod = await ProductModel.find().populate({
+    path: "category",
+    select: "name -_id",
+  });
+  if (!prod) {
+    res.status(200).json({
+      status: "success",
+      data: {},
+    });
+  }
+  res.status(200).json({
+    status: "success",
+    data: prod,
+  });
+});
+
+router.get("/:productId", async (req, res, next) => {
+  const prod = await ProductModel.findById(req.params.productId).populate({
+    path: "category",
+    select: "name -_id",
+  });
   if (!prod) {
     res.status(200).json({
       status: "success",
@@ -17,20 +39,108 @@ router.get("/", async (req, res, next) => {
 });
 
 router.post("/", async (req, res, next) => {
-  const product = req.body;
   try {
-    const newProduct = await ProductModel.create(product);
-    if (newProduct) {
-      res.status(200).json({
-        status: "Success",
-        data: newProduct,
+    const category = await CategoryModel.findById(req.body.category);
+    if (!category)
+      return res
+        .status(500)
+        .json({ status: "Fail", message: "Invalid Category!" });
+    const newProduct = await ProductModel.create({
+      name: req.body.name,
+      description: req.body.description,
+      richDestination: req.body.richDestination,
+      image: req.body.image,
+      images: req.body.images,
+      brand: req.body.brand,
+      price: req.body.price,
+      category: req.body.category,
+      countInStock: req.body.countInStock,
+      rating: req.body.rating,
+      isFeatured: req.body.isFeatured,
+      dateCreated: req.body.dateCreated,
+    });
+    if (!newProduct)
+      return res.status(500).json({
+        status: "Fail",
+        message: err.message,
       });
-    }
+    res.status(201).json({
+      status: "Success",
+      data: newProduct,
+    });
   } catch (err) {
     res.status(500).json({
+      status: "Success",
+      data: newProduct,
+    });
+  }
+});
+
+router.patch("/:productId", async (req, res, next) => {
+  try {
+    if (req.body.category) {
+      const category = await CategoryModel.findById(req.body.category);
+      if (!category) {
+        return res
+          .status(500)
+          .json({ status: "Fail", message: "Invalid Category!" });
+      }
+    }
+    const updatedProduct = await ProductModel.findByIdAndUpdate(
+      { _id: req.params.productId },
+      {
+        name: req.body.name,
+        description: req.body.description,
+        richDestination: req.body.richDestination,
+        image: req.body.image,
+        images: req.body.images,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        isFeatured: req.body.isFeatured,
+        dateCreated: req.body.dateCreated,
+      },
+      { new: true }
+    );
+    if(updatedProduct)
+        res.status(201).json({
+          status: "Success",
+          data: updatedProduct,
+        });
+        if(!updatedProduct)
+        res.status(500).json({
+          status: "Faild",
+          message:"Faild to update the product"
+        });
+  } catch (err) {
+    res.status(500).json({
+      status: "Fail",
+      message: err.message ,
+    });
+  }
+});
+
+router.delete("/:productId", async (req, res, next) => {
+  if(!mongoose.isValidObjectId(req.params.productId)){
+    res.status(400).json({
+      status:"fail",
+      message:"invalid id"
+    })
+  }
+  try {
+   const product = await ProductModel.findByIdAndDelete(req.params.productId);
+   if(!product){
+      return res.status(404).send("No product found with that id!")
+   }
+   res.status(204).json({status:"success", data:null})
+   } catch (err) {
+    res.status(404).json({
       status: "Fail",
       message: err.message,
     });
   }
 });
+
 module.exports = router;
