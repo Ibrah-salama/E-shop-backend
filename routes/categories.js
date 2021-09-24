b4763bd3805ea9180de0b15102cc2b0f7dd45c8e
i-salama <ibrah.salama@outlook.com>
2021-09-24 04:50:50 +0200

    Using git merge with main and routes .. routes has 2 commits and main has one

--- routes/categories.js
+++ routes/categories.js
@@ -1,11 +1,39 @@
 const express = require('express')
+const CategoryModel = require('../models/category')
 const router = express.Router()
 
 router.get('/', async(req,res,next)=>{
-
+    
+    const categories = await CategoryModel.find()
+    if(!categories){
+        res.status(500).json({
+            status: Fail 
+        })
+    }
     res.status(200).json({
-        status:'success'
+        status:'success',
+        data : categories
     })
+})
+
+router.post("/", async(req,res,next)=>{
+    try{ 
+        const category = req.body 
+        const newCategory = await CategoryModel.create(category)
+        if(newCategory){
+            res.status(201).json({
+                status:"success",
+                data: newCategory
+            })
+        }
+    }catch(err){
+        res.status(500).json({
+            status:"Fail",
+            message: err.message
+        })
+    }
+
+
 })
 
 module.exports = router 
