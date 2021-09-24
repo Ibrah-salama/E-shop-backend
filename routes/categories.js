bb2d2ae0a041b66295669977fd617c58f50f691c
i-salama <ibrah.salama@outlook.com>
2021-09-24 05:28:22 +0200

    Add Get specific category route

--- routes/categories.js
+++ routes/categories.js
@@ -1,63 +1,82 @@
-const express = require('express')
-const CategoryModel = require('../models/category')
-const router = express.Router()
+const express = require("express");
+const CategoryModel = require("../models/category");
+const router = express.Router();
 
-router.get('/', async(req,res,next)=>{
-    
-    const categories = await CategoryModel.find()
-    if(!categories){
-        res.status(500).json({
-            status: Fail 
-        })
+router.get("/", async (req, res, next) => {
+  const categories = await CategoryModel.find();
+  if (!categories) {
+    res.status(500).json({
+      status: Fail,
+    });
+  }
+  res.status(200).json({
+    status: "success",
+    data: categories,
+  });
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const category = req.body;
+    const newCategory = await CategoryModel.create(category);
+    if (newCategory) {
+      res.status(201).json({
+        status: "success",
+        data: newCategory,
+      });
     }
-    res.status(200).json({
-        status:'success',
-        data : categories
-    })
-})
+  } catch (err) {
+    res.status(400).json({
+      status: "Fail",
+      message: err.message,
+    });
+  }
+});
 
-router.post("/", async(req,res,next)=>{
-    try{ 
-        const category = req.body 
-        const newCategory = await CategoryModel.create(category)
-        if(newCategory){
-            res.status(201).json({
-                status:"success",
-                data: newCategory
-            })
-        }
-    }catch(err){
-        res.status(400).json({
-            status:"Fail",
-            message: err.message
-        })
+router.get("/:categoryId", async (req, res, next) => {
+  const categoryId = req.params.categoryId;
+  try {
+    const category = await CategoryModel.findById({ _id: categoryId });
+    if (category) {
+      res.status(200).json({
+        status: "success",
+        data: category,
+      });
+    } else {
+      res.status(401).json({
+        status: "Fail",
+        message: "Category not found!",
+      });
     }
-})
+  } catch (err) {
+    res.status(400).json({
+      status: "Fail",
+      message: err.message,
+    });
+  }
+});
 
-router.delete("/:categoryId", async(req,res,next)=>{
-    try{
-        const categoryId = req.params.categoryId 
-        const category = await CategoryModel.findByIdAndDelete({_id : categoryId})
-        if(category){ 
-            res.status(204).json({
-                status:"success",
-                message : "Category deleted successfully!"
-            })
-        }else{ 
-            res.status(404).json({
-                status:"Fail",
-                message : "Category not found!"
-            })
-        }
-    }catch(err){
-        res.status(401).json({
-            status:"Fail",
-            message: err.message
-        })
+router.delete("/:categoryId", async (req, res, next) => {
+  try {
+    const categoryId = req.params.categoryId;
+    const category = await CategoryModel.findByIdAndDelete({ _id: categoryId });
+    if (category) {
+      res.status(204).json({
+        status: "success",
+        message: "Category deleted successfully!",
+      });
+    } else {
+      res.status(401).json({
+        status: "Fail",
+        message: "Category not found!",
+      });
     }
+  } catch (err) {
+    res.status(404).json({
+      status: "Fail",
+      message: err.message,
+    });
+  }
+});
 
-
-
-})
-
-module.exports = router 
+module.exports = router;
